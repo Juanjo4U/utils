367770f11ba6ee9627e9722bd30514bd233b2fab
Juanjo4U <juanjos.xf@gmail.com>
2021-07-18 09:47:06 +0200

    object comparison improved

--- src/utils/validations/values/index.js
+++ src/utils/validations/values/index.js
@@ -1,24 +1,39 @@
 import { isDateEqual } from "../date";
+import { isInstanceOf } from "../instance";
 import { isLengthEqual } from "../length";
+import { isParamsArrayType, isParamsObjectType } from "../type";
 
 //////// UTILS
 const isValueEqual = (a, b) => a === b
 
-////////  OBJECT
-const isObjectValuesEqual = (obj1, obj2) => {
-    if (typeof obj1 !== 'object' || typeof obj1 !== typeof obj2) throw new Error('all params has to be type object');
-    let isEqual = true && isLengthEqual(obj1, obj2);
-    for (const key in obj1) {
-        if (!isEqual) return false;
-        let a = obj1[key], b = obj2[key];
-        if (typeof a !== typeof b || b === undefined) return false
-        isEqual = typeof a === 'object' ? (
-            a instanceof Date ? (b instanceof Date ? isDateEqual(a, b) : false) :
-                a instanceof RegExp ? (b instanceof RegExp ? a.source === b.source : false) :
-                    isObjectValuesEqual(a, b)
-        ) : isValueEqual(a, b)
+export const isSameInstanceValue = (a, b) => {
+    switch (true) {
+        case isInstanceOf(RegExp, a, b):
+            return a.source === b.source;
+        case isInstanceOf(Date, a, b):
+            return isDateEqual(a, b);
+        default: return false
     }
-    return isEqual
+}
+
+////////  OBJECT
+export const isObjectValuesEqual = (obj1, obj2) => {
+    if (typeof obj1 !== 'object' || typeof obj1 !== typeof obj2)
+        throw new Error('all params has to be type object');
+    if (isParamsObjectType(obj1, obj2) || isParamsArrayType(obj1, obj2)) {
+        let isEqual = true && isLengthEqual(obj1, obj2);
+        for (const key in obj1) {
+            if (!isEqual) return false;
+            const a = obj1[key], b = obj2[key];
+            if (typeof a !== typeof b) return false
+            isEqual = typeof a === 'object' ? (
+                (a instanceof Date || b instanceof Date) || (a instanceof RegExp || b instanceof RegExp) ?
+                    isSameInstanceValue(a, b) :
+                    isObjectValuesEqual(a, b)
+            ) : isValueEqual(a, b)
+        }
+        return isEqual
+    } else return false
 }
 
 export const isParamsValuesEqualTo = (value, ...args) => {
@@ -26,11 +41,9 @@ export const isParamsValuesEqualTo = (value, ...args) => {
     for (const val of args) {
         if (!isEqual) return false
         isEqual = typeof val === 'object' ? (
-            val instanceof Date ?
-                (value instanceof Date ? isDateEqual(val, value) : false) :
-                val instanceof RegExp ?
-                    (value instanceof RegExp ? val.source === value.source : false) :
-                    isObjectValuesEqual(val, value)
+            (value instanceof Date || val instanceof Date) || (value instanceof RegExp || val instanceof RegExp) ?
+                isSameInstanceValue(value, val) :
+                isObjectValuesEqual(value, val)
         ) : isValueEqual(val, value)
     }
     return isEqual
